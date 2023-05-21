9eba687ad14e1627c7f43c8525f66fb47cb21473
Judit Vives <jvivesramon@gmail.com>
2023-05-21 15:01:54 +0200

    Add Layout component and LayoutStyled component

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+import LayoutStyled from "./LayoutStyled";
+import Header from "../Header/Header";
+
+const Layout = (): JSX.Element => {
+  return (
+    <LayoutStyled className="main-container">
+      <Header />
+      <Outlet />
+    </LayoutStyled>
+  );
+};
+
+export default Layout;
