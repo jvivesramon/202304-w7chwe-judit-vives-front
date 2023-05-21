f3d7f94e80f109a4331c161bb7cdbfb30ad330f1
Judit Vives <jvivesramon@gmail.com>
2023-05-21 15:12:32 +0200

    Add HomePage component

---
+++ src/components/pages/HomePage/HomePage.tsx
@@ -0,0 +1,19 @@
+import LoginForm from "../../LoginForm/LoginForm";
+
+const HomePage = (): JSX.Element => {
+  return (
+    <>
+      <img
+        className="header-icon"
+        src="/images/ISDEEPWEB.svg"
+        alt="Isdeepweb logo"
+        width="320"
+        height="285.60"
+      />
+      <h2 className="info-title">Once you are in, you will stay forever</h2>
+      <LoginForm />
+    </>
+  );
+};
+
+export default HomePage;
