2d1cf7edb611bb560127744c94e2b5548b01a3e3
Judit Vives <jvivesramon@gmail.com>
2023-05-23 00:27:09 +0200

    Add route to navigate from LoginPage to Contacts page

---
+++ src/components/pages/ContactsPage/ContactsPage.tsx
@@ -0,0 +1,12 @@
+import UserCard from "../../userCard/UserCard";
+
+const ContactsPage = (): JSX.Element => {
+  return (
+    <>
+      <h1>OS ODIO A TODOS</h1>
+      <UserCard img="" name="" />
+    </>
+  );
+};
+
+export default ContactsPage;
